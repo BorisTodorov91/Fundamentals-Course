fc9f954bfaf83e2eb59176088038d3f10c6ac99e
Boris <b.todorov91@gmail.com>
2022-06-07 22:03:59 +0300

    new update

---
+++ src/ArraysLab/Demo.java
@@ -0,0 +1,23 @@
+package ArraysLab;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int [] arr = new int[n];
+
+        //пълнене на масива
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = Integer.parseInt(scanner.nextLine());
+        }
+
+        //принтиране на масива
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
---
+++ src/ArraysLab/P01DayofWeek.java
@@ -0,0 +1,18 @@
+package ArraysLab;
+
+import java.util.Scanner;
+
+public class P01DayofWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       String [] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+       int n = Integer.parseInt(scanner.nextLine());
+
+        if (n >=1 && n <=7){
+            System.out.println(days[n-1]);
+        }else {
+            System.out.println("Invalid day!");
+        }
+    }
+}
