dafd9cd4fc6bbca86f9bc013b41469c221853308
Boris <b.todorov91@gmail.com>
2022-06-11 17:16:52 +0300

    new update

--- src/ExerciseArrays/P04ArrayRotation.java
+++ src/ExerciseArrays/P04ArrayRotation.java
@@ -15,5 +15,28 @@ public class P04ArrayRotation {
           2. 32 61 21 51 47
         */
 
+
+        String [] arrOfNum = scanner.nextLine().split(" ");
+        int numOfRotation = Integer.parseInt(scanner.nextLine());
+        String lastIndex = arrOfNum[arrOfNum.length - 1];
+        for (int i = 1; i <=numOfRotation ; i++) {
+            //1. ротация
+            //2. взимаме първия елемент на масива
+            String firstElement = arrOfNum[0];
+
+            //3. местиме всички елементи наляво
+            for (int index = 0; index < arrOfNum.length -1; index++) {
+                arrOfNum[index] = arrOfNum[index +1];
+            }
+            //4. слагаме последния елемент на масива на индекс 0
+            arrOfNum[arrOfNum.length - 1] = firstElement;
+
+        }
+
+
+        for (String num: arrOfNum) {
+            System.out.print(num + " ");
+        }
+
     }
 }
