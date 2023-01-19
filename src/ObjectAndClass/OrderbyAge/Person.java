e70902b1b3c9ea0b253a5bb83146bde27c780804
Boris <b.todorov91@gmail.com>
2023-01-19 19:47:31 +0200

    new tast

---
+++ src/ObjectAndClass/OrderbyAge/Person.java
@@ -0,0 +1,42 @@
+package ObjectAndClass.OrderbyAge;
+
+public class Person {
+    private String name;
+    private int id;
+    private int age;
+
+    public Person(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s with ID: %d is %d years old.", this.name, this.id,  this.age);
+    }
+}
---
+++ src/ObjectAndClass/OrderbyAge/main.java
@@ -0,0 +1,33 @@
+package ObjectAndClass.OrderbyAge;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String comand = scanner.nextLine();
+
+        List<Person> personList = new ArrayList<>();
+        while (!comand.equals("End")){
+            String[] data = comand.split("\\s+");
+
+            String name = data[0];
+            int id = Integer.parseInt(data[1]);
+            int age = Integer.parseInt(data[2]);
+
+            Person person = new Person(name, id,age);
+
+            personList.add(person);
+
+            comand = scanner.nextLine();
+        }
+
+        //personList.stream().sorted((first, second) -> Integer.compare(first.getAge(), second.getAge()) ).forEach(System.out::println);
+        personList.stream().sorted(Comparator.comparing(Person::getAge)).forEach(System.out::println);
+    }
+}
